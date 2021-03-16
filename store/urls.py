7defd086ff0d548b4fd6d5faf7e463c017ea654a
Shohruxshoh <shohhhhhhhhhhhhhhhhhhhh@gmail.com>
2021-03-16 22:18:36 +0500

    ecommerce

---
+++ store/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+
+
+
+urlpatterns = [
+    path('', views.store, name="store"),
+    path('card/', views.card, name="card"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.updateItem, name="process_order"),
+    
+]
